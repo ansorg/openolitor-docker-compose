ee98818fcca6b5990d5a5c4e42a5ff2d480ab5d1
Jens <ansorg@users.noreply.github.com>
2023-03-06 09:51:26 +0100

    Update config.js

--- config/client/admin/config.js
+++ config/client/admin/config.js
@@ -2,8 +2,8 @@
 
 function getConfig() {
   return {
-    "API_URL": "http://localhost:8080/api-csa1/",
-    "API_WS_URL": "http://localhost:8080/api-csa1/ws",
+    "API_URL": "https://ool.ax9.eu/api-csa1/",
+    "API_WS_URL": "https://ool.ax9.eu/api-csa1/ws",
     "ENV": "dev",
     "version": "2.4.0",
     "EMAIL_TO_ADDRESS": "info@tegonal.com",
